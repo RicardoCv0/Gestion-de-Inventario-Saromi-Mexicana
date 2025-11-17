fdc22cf4c19a0592a276a751e46178030292024d
JoseLuisLaraRubio <a22216373@alumnos.uady.mx>
2025-11-16 18:52:45 -0600

    create inventory models

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Gemstone(models.Model):
+    id = models.CharField(max_length=5)
+    name = models.CharField(max_length=100)
+    ammount_available = models.DecimalField
+
+class InventoryMovement(models.Model):
+    gemstone = models.ForeignKey(Gemstone, on_delete=models.DO_NOTHING)
+    ammount = models.DecimalField
+    date_time = models.DateTimeField
+    responsable = models.CharField(max_length=100)
+
+class EntryMovement(InventoryMovement):
+    pass
+
+class ExitMovement(InventoryMovement):
+    destination = models.CharField(max_length=200)
+
+class AdjustmentMovement(InventoryMovement):
+    motive = models.CharField(max_length=200)
