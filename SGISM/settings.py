f8cf9bae6afa2eb149842a621ac04dd6862fc267
JoseLuisLaraRubio <a22216373@alumnos.uady.mx>
2025-11-16 18:56:50 -0600

    Add inventory and users config to settings.py

--- SGISM/settings.py
+++ SGISM/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'inventory.apps.InventoryConfig'
+    'users.apps.UsersConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
