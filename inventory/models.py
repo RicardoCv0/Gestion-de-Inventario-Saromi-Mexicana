d65e5a16f0d214ced3705dc07810f4d82a1c6d9b
JoseLuisLaraRubio <a22216373@alumnos.uady.mx>
2025-11-17 00:05:38 -0600

    create function definitions for inventory views

--- inventory/models.py
+++ inventory/models.py
@@ -4,12 +4,12 @@ class Gemstone(models.Model):
     id = models.CharField(max_length=5, primary_key=True)
     type = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
-    ammount_available = models.DecimalField
+    ammount_available = models.DecimalField(max_digits=20, decimal_places=5, default=0)
 
 class InventoryMovement(models.Model):
     gemstone = models.ForeignKey(Gemstone, on_delete=models.DO_NOTHING)
-    ammount = models.DecimalField
-    date_time = models.DateTimeField
+    ammount = models.DecimalField(max_digits=20, decimal_places=5)
+    date_time = models.DateTimeField()
     responsible = models.CharField(max_length=100)
 
 class EntryMovement(InventoryMovement):
--- inventory/views.py
+++ inventory/views.py
@@ -3,4 +3,24 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hi!!")
+    return HttpResponse("Hi!!, This is the inventory dashboard")
+
+# Gemstones Crud
+def add_gemstone(request):
+    return HttpResponse("Hellope!!, You are adding a new gemstone type")
+
+def edit_gemstone(request, gemstone_id):
+    return HttpResponse("Howdy!!, You are editing the data for the %s gemstone" % gemstone_id)
+
+def delete_gemstone(request, gemstone_id):
+    return HttpResponse("Oi!!, You are you are deleting the %s gemstone" % gemstone_id)
+
+# Gemstones Movement
+def entry_movement(request, gemstone_id):
+    return HttpResponse("Hellope!!, You are you are adding to the %s gemstone" % gemstone_id)
+
+def adjustment_movement(request, gemstone_id):
+    return HttpResponse("Howdy!!, You are you are adjusting the %s gemstone" % gemstone_id)
+
+def exit_movement(request, gemstone_id):
+    return HttpResponse("Oi!!, You are you are removing from the %s gemstone" % gemstone_id)
