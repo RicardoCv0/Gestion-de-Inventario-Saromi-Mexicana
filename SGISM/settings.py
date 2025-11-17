9d0c834157707b0d43b51f62dc75c3d5baff87c9
JoseLuisLaraRubio <a22216373@alumnos.uady.mx>
2025-11-16 19:00:14 -0600

    fixed typo

--- SGISM/settings.py
+++ SGISM/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'inventory.apps.InventoryConfig'
-    'users.apps.UsersConfig'
+    'inventory.apps.InventoryConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
